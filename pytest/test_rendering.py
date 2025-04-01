8b6f5e72acdaac4e2efec90aa0d68d93b9429c55
Aapo <aapokossi@gmail.com>
2025-04-01 11:31:43 +0200

    Fix rpath and add xfail to rendering test if it can be deduced that no video output is available

--- pytest/test_rendering.py
+++ pytest/test_rendering.py
@@ -1,25 +1,35 @@
 import numpy as np
 import city_of_gold as cg
 
-def test_renderloop():
+import pytest
 
-    env = cg.cog_env(123, 4, 4, cg.Difficulty.HARD, 100, True)
-    sampler = cg.action_sampler(987)
-    obs = cg.ObsData()
-    act = cg.ActionData()
-    mask = cg.ActionMask()
-    info = cg.Info()
-    rew = np.zeros(4)
-    env.init(obs, info, rew, mask)
-    env.reset()
-    env.render()
-    step = 0
-    while (step != 1000):
-        if env.get_done():
-            env.reset()
-        act = sampler.sample(mask)
-        env.step(act)
+def test_renderloop(capsys):
+
+    try:
+        env = cg.cog_env(123, 4, 4, cg.Difficulty.HARD, 100, True)
+        sampler = cg.action_sampler(987)
+        obs = cg.ObsData()
+        act = cg.ActionData()
+        mask = cg.ActionMask()
+        info = cg.Info()
+        rew = np.zeros(4)
+        env.init(obs, info, rew, mask)
+        env.reset()
         env.render()
-        step += 1
+        step = 0
+        while (step != 1000):
+            if env.get_done():
+                env.reset()
+            act = sampler.sample(mask)
+            env.step(act)
+            env.render()
+            step += 1
 
+        assert True
+    except RuntimeError:
+        err = capsys.readouterr().err
+        if "No available video device" in err:
+            pytest.xfail(reason=err)
     return
+
+
